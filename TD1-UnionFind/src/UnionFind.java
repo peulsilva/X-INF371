7399baa62187b95b9637c8f3f5ebb3b181eef0df
peulsilva <peulsilva9@gmail.com>
2023-04-25 17:46:15 +0200

    optimize Union and Find methods

--- TD1-UnionFind/src/UnionFind.java
+++ TD1-UnionFind/src/UnionFind.java
@@ -1,14 +1,33 @@
 public class UnionFind {
     static int[] equiv;
+    static int[] height;
 
     public static void init(int len) {
-        equiv = new int[len];
-        for (int i = 0; i< equiv.length; i++)
+        equiv = new int[len + 2];
+        height = new int [len + 2];
+        for (int i = 0; i< equiv.length; i++){
             equiv[i] = i;
+            height[i] = 1;
+        }
     }
 
     public static int naiveFind(int x){
         return equiv[x];
+    }
+
+    public static int fastFind(int x){
+        while (equiv[x] != x){
+            x = equiv[x];
+        }
+        return x;
+    }
+
+    public static int logFind(int x){
+        while (equiv[x] != x){
+            equiv[x] = equiv[equiv[x]];
+            x = equiv[x];
+        }
+        return x;
     }
 
     public static int naiveUnion(int x, int y){
@@ -20,12 +39,42 @@ public class UnionFind {
         return equiv[y];
     }
 
+    public static int fastUnion(int x, int y){
+        int rootY = fastFind(y);
+        int rootX = fastFind(x);
+
+        x = rootX;
+        y = rootY;
+
+        equiv[x] = rootY;
+        return rootY;
+    }
+
+    public static int logUnion(int x, int y){
+        if (height[x] > height[y]){
+            return fastUnion(y, x);
+        }
+        else if (height[y] > height[x]){
+            return fastUnion(x, y);
+        }
+        else { 
+            // height[x] == height[y]
+            height[y] += 1;
+            return fastUnion(x, y);
+        } 
+
+    }
+
     public static int find(int x){
-        return naiveFind(x);
+        // return naiveFind(x);
+        // return fastFind(x); 
+        return logFind(x);
     }
 
     public static int union(int x, int y){
-        return naiveUnion(x, y);
+        // return naiveUnion(x, y);
+        // return fastUnion(x, y);
+        return logUnion(x, y);
     }
 
     public static void main(String[] args){
