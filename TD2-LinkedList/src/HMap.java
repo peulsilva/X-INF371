5955cf7b67eaeea496b1ec4ec6809c429de883b7
peulsilva <peulsilva9@gmail.com>
2023-05-11 17:34:07 +0200

    change addsimple method

--- TD2-LinkedList/src/HMap.java
+++ TD2-LinkedList/src/HMap.java
@@ -12,26 +12,47 @@ public class HMap {
         this.t = new EntryList[20];
     }
 
-    WordList find(Prefix key){
+    public WordList find(Prefix key){
         int hashIdx = key.hashCode(this.t.length);
         if (this.t[hashIdx] == null)
             return null;
 
-        WordList found = new WordList();
-
         for (EntryList curr = this.t[hashIdx]; curr != null; curr = curr.next){
-            found = new WordList(curr.head.value.toString(), found.content);
-        } 
-        return found; 
+            if (Prefix.eq(key, curr.head.key))
+                return curr.head.value;
+        }
+        return null;
+        
     }
 
     void addSimple(Prefix key, String w){
+        boolean isInserted = false;
         int hashIdx = key.hashCode(this.t.length);
+
         if (this.t[hashIdx] != null){
-            this.t[hashIdx]
-                .head
-                .value
-                .addLast(w);
+            for (EntryList curr = this.t[hashIdx]; curr != null; curr = curr.next){
+                if (Prefix.eq(key, curr.head.key)){
+                    curr.head
+                        .value
+                        .addLast(w);
+
+                    isInserted = true;
+                }
+            }
+
+            if (!isInserted){
+                EntryList lastElement = this.t[hashIdx];
+                while (lastElement.next != null)
+                    lastElement = lastElement.next;
+
+                lastElement.next = new EntryList(
+                    new Entry(key, new WordList(w, null)), 
+                    null
+                );
+
+                this.nbEntries++;
+               
+            }
         }
         else { // this.t[hashIdx] == null
             this.t[hashIdx] = new EntryList(
@@ -48,12 +69,11 @@ public class HMap {
         for (int i = 0; i < this.t.length; i++){
             if (this.t[i] == null)
                 continue;
-
+            String str = "\t" + i+ ": ";
             for (EntryList curr = this.t[i]; curr != null; curr = curr.next){
-                String str = "\t";
-                str += Arrays.toString(curr.head.key.t)+ ": "+ curr.head.value.toString();
-                System.out.println(str);
+                str += "{" + Arrays.toString(curr.head.key.t)+ ": "+ curr.head.value.toString() + "}";
             }
+            System.out.println(str);
             
         }
         System.out.println("}");
@@ -62,14 +82,13 @@ public class HMap {
     void rehash(int n){
         HMap rehashedMap = new HMap(n);
         for (int i = 0; i < this.t.length; i++){
-            if (this.t[i] == null)
-                continue;
 
             for (EntryList curr = this.t[i]; curr != null; curr = curr.next){
-                rehashedMap.addSimple(
-                    curr.head.key, 
-                    curr.head.value.toString()
-                );
+                for (Node word = curr.head.value.content; word != null; word = word.next)
+                    rehashedMap.addSimple(
+                        curr.head.key, 
+                        word.head
+                    );
             }
         }
 
@@ -78,24 +97,36 @@ public class HMap {
     }
 
     void add(Prefix key, String w ){
-        if (this.nbEntries > 0.75 * this.t.length)
+        if (this.nbEntries >= 0.75 * this.t.length){
             rehash(2*this.t.length);
+        }
         
         addSimple(key, w);
     }
 
     public static void main(String[] args){
-        HMap dict = new HMap();
+        HMap dict = new HMap(1);
         
         // dict.print();
-        dict.addSimple(new Prefix(new String[] {"hello"}), "world");
-        dict.addSimple(new Prefix(new String[] {"hello"}), "france");
-        dict.addSimple(new Prefix(new String[]{"I", "am", "a"}), "student");
-        // dict.print();
-        System.out.println(
-            dict
-            .find(new Prefix(new String[] {"hello"}))
-            .toString()
-        );
+        // System.out.println(new Prefix(new String[] {"hello"}).hashCode(20));
+        
+        dict.add(new Prefix(new String[] {"hello"}), "world");
+        dict.add(new Prefix(new String[] {"hello"}), "france");
+        dict.add(new Prefix(new String[] {"Aa"}), "word1");
+        dict.add(new Prefix(new String[] {"BB"}), "word2");
+        dict.add(new Prefix(new String[] {"BB"}), "word2.1");
+        dict.add(new Prefix(new String[]{"I", "am", "a"}), "student");
+        dict.add(new Prefix(new String[] {"x"}), "word2");
+        dict.add(new Prefix(new String[] {"y"}), "word2");
+        dict.add(new Prefix(new String[] {"BB"}), "d");
+        dict.add(new Prefix(new String[] {"f"}), "tr");
+        dict.add(new Prefix(new String[] {"BB"}), "ewr");
+        dict.print();
+
+        // System.out.println(
+        //     dict
+        //     .find(new Prefix(new String[] {"BB"}))
+        //     .toString()
+        // );
     }
 }
