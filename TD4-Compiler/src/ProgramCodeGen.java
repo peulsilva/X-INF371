3a7ba4ad95286fa14880d46f634d7ed07172760c
peulsilva <peulsilva9@gmail.com>
2023-05-30 22:38:03 +0200

    change offset: negative for arguments and positive for local variables

--- TD4-Compiler/src/ProgramCodeGen.java
+++ TD4-Compiler/src/ProgramCodeGen.java
@@ -3,39 +3,44 @@ import java.util.List;
 import edu.polytechnique.mjava.ast.TProcDef;
 import edu.polytechnique.mjava.ast.VarDecl;
 import edu.polytechnique.xvm.asm.opcodes.GSB;
+import edu.polytechnique.xvm.asm.opcodes.PUSH;
 import edu.polytechnique.xvm.asm.opcodes.RET;
 import edu.polytechnique.xvm.asm.opcodes.STOP;
 
 public class ProgramCodeGen {
   public final CodeGen cg = new CodeGen();
-  int offset = 0;
-
+  
   public static String labelOfProcName(String name) {
     return String.format("__%s", name);
   }
-
+  
   @SuppressWarnings("unused")
   public void codegen(TProcDef<AbstractExpr, AbstractInstruction> proc) {
     final List<VarDecl> args = proc.getArgs(); // Proc. arguments
     final List<VarDecl> locals = proc.getLocals(); // Proc. locals
     final AbstractInstruction is = proc.getBody(); // Proc. body
-
     
+    // for function arguments, the addres related to the FP is -1, -2, ..., -n
+    int offset = -args.size();
     String procName = ProgramCodeGen
       .labelOfProcName(proc.getName());
 
     cg.pushLabel(procName);
     
     for (VarDecl arg : args){
-      this.offset++;
       String argName = arg.getName();
-      cg.pushLocalVariable(argName, this.offset);
+      cg.pushLocalVariable(argName, offset);
+      offset ++;
     }
 
+    // for local variables, the address related to the FP is 2, 3, ..., n+2
+    offset = 2;
     for (VarDecl localVar: locals){
-      this.offset++;
+      
       String varName = localVar.getName();
-      cg.pushLocalVariable(varName, this.offset); 
+      cg.pushLocalVariable(varName, offset); 
+      cg.pushInstruction(new PUSH(0));
+      offset ++;
     }
 
     is.codegen(cg);
